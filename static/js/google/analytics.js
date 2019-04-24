a2f5a634bf2a1d689083787083774750c9fc4375
여상준 (YeoSJ116) <33376555+YeoSJ116@users.noreply.github.com>
2019-04-24 21:10:20 +0900

    update

--- static/js/google/analytics.js
+++ static/js/google/analytics.js
@@ -6,5 +6,5 @@
 		a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})
 (window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-138900801-1', 'auto');
+ga('create', '{{ site.google_analytics }}', 'auto');
 ga('send', 'pageview');
