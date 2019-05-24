03ec2b58c1f8af7cbee21ea142a8489787a8102c
여상준 (YeoSJ116) <33376555+YeoSJ116@users.noreply.github.com>
2019-05-24 23:28:18 +0900

    update

---
+++ static/js/browserGuider.js
@@ -0,0 +1,29 @@
+(function($){
+	$(document).ready(function(){
+		if(getBrowserType() == "MSIE")
+			$('.browserGuider').removeClass("not-show");
+
+	$.fn.extend({
+	  getBrowserType: function() { //실질적으로 프로젝트 표시 유무를 판단하는 함수
+	    var browser = "Unknown";
+		if(navigator.appName.charAt(0) == "N"){ //Netscape
+			if(navigator.userAgent.indexOf("Chrome") != -1)
+				browser = "Chrome";
+			else if(navigator.userAgent.indexOf("Friefox") != -1)
+				browser = "Friefox";
+			else if(navigator.userAgent.indexOf("Safari") != -1)
+				browser = "Safari";
+			else if(navigator.userAgent.indexOf("Opera") != -1)
+				browser = "Opera";
+			else if(navigator.userAgent.indexOf("Trident") != -1)
+				browser = "MSIE"; // IE11 above (Trident)
+		}
+		else if(navigator.appName.charAt(0) == "M")
+			browser = "MSIE";
+		else if(navigator.appName.charAt(0) == "O")
+			browser = "Opera";
+		console.log("Your browser is ", browser); 
+		return browser;
+	    });
+	});
+})(jQuery)
