c401b2436e2a81c1875ec9d9ef509f5a1f633a98
여상준 (YeoSJ116) <33376555+YeoSJ116@users.noreply.github.com>
2019-05-25 00:26:36 +0900

    Update browserGuider.js

--- static/js/browserGuider.js
+++ static/js/browserGuider.js
@@ -1,34 +1,27 @@
 (function($){
 	$(document).ready(function(){
-		if($.browser.chrome) {
-			alert(1);
-		} else if ($.browser.mozilla) {
-			alert(2);
-		} else if ($.browser.msie) {
-			alert(3);
-		}
-
 		var browser = "Unknown";
-		var appName = navigator.userAgent.toLowerCase();
-		if(navigator.appName.charAt(0) == "n"){ //Netscape
-			if(navigator.userAgent.indexOf("chrome") != -1)
-				browser = "Chrome";
-			else if(navigator.userAgent.indexOf("friefox") != -1)
-				browser = "Friefox";
-			else if(navigator.userAgent.indexOf("safari") != -1)
-				browser = "Safari";
-			else if(navigator.userAgent.indexOf("opera") != -1)
-				browser = "Opera";
-			else if(navigator.userAgent.indexOf("trident") != -1)
-				browser = "MSIE"; // IE11 above (Trident)
+		var userAgent = navigator.userAgent.toLowerCase();
+		var appName = navigator.appName.toLowerCase();
+		if(appName == "netscape"){ //Netscape
+			if(userAgent == "chrome")
+				browser = "chrome";
+			else if(userAgent == "friefox")
+				browser = "friefox";
+			else if(userAgent == "safari")
+				browser = "safari";
+			else if(userAgent == "opera")
+				browser = "opera";
+			else if(userAgent == "trident")
+				browser = "msie"; // IE11 above (Trident)
 		}
-		else if(navigator.appName.charAt(0) == "m")
-			browser = "MSIE";
-		else if(navigator.appName.charAt(0) == "o")
-			browser = "Opera";
+		else if(appName == "msie")
+			browser = "msie";
+		else if(appName == "opera")
+			browser = "opera";
 		console.log("Your browser is ", browser);
 
-		if(browser == "MSIE")
+		if(browser == "msie")
 			$('.browserGuider').removeClass("not-show");
 	});
 })(jQuery);
